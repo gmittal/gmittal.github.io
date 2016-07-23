cb9329d740d1904520c30e38a694f78010b9ef9d
gmittal <gautam@mittal.net>
2016-07-22 17:53:37 -0700

    Fixed size issue with Twitter widget.

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,14 @@
 var greetings = ["Hi", "Hello", "Hey"]
 
-$(window).resize(function() {
+function initTwitter() {
 	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
-});
+}
 
 $(document).ready(function() {
-	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
 	var greeting = greetings[Math.floor(Math.random()*greetings.length)+0];
 	$("greeting").text(greeting);
+});
+
+$(window).resize(function() {
+	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
 });
