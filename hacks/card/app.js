a08157d7c45ff1b260b42dd7d23311d83422afba
gmittal <gautam@mittal.net>
2017-12-02 20:15:08 -0800

    Refactoring various hacks I've built over the years.

--- hacks/card/app.js
+++
@@ -1,9 +0,0 @@
-var quotes = [
-    ['[Luke:] I can\'t believe it! [Yoda:] That is why you fail.', 'Master Yoda'],
-    ['Do or do not. There is no try.', 'Master Yoda'],
-    ['Be the change you wish to see in the world.', 'Mahatma Gandhi'],
-];
-
-var quote = quotes[Math.floor(Math.random() * quotes.length)];
-document.querySelector(".quote").innerText = quote[0];
-document.querySelector("cite").innerText = "--" + quote[1];
