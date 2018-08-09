25f2e83ebf794deb0edafdd8e1fc7f66a049a7c2
Gautam Mittal <gautam@mittal.net>
2018-08-08 17:16:45 -0700

    Remove all JS.

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function initTwitter() {
-	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
-}
-
-$(window).resize(function() {
-	$(".twitter-timeline").height($("#pictureOfMe").height()+20);
-});
